84dbf878b71a898df580d177877959fb10ec0d5f
1f10220090 <s1f102200907@iniad.org>
2023-07-04 12:35:17 +0900

    トップページを追加2

--- todo/views.py
+++ todo/views.py
@@ -5,9 +5,9 @@ from todo.models import Task
 
 # Create your views here.
 def index(request):
-    if request.method == 'POAT':
-        task = Task(title=request.POAT['title'],
-                    due_at=make_aware(parse_datetime(request.POAT['due_at'])))
+    if request.method == 'POST':
+        task = Task(title=request.POST['title'],
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
 
     tasks = Task.objects.all()
