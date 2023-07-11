ae6302307a143c723b27351264b9f44963b9d9ac
1f10220090 <s1f102200907@iniad.org>
2023-07-11 10:57:52 +0900

    警告を修正2

--- todo/tests.py
+++ todo/tests.py
@@ -8,6 +8,7 @@ class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1+2, 3)
 
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
@@ -50,6 +51,7 @@ class TaskModelTestCase(TestCase):
         task.save()
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -61,7 +63,7 @@ class TodoViewTestCase(TestCase):
 
     def test_index_post(self):
         client = Client()
-        data = {'title':'Test Task', 'due_at': '2023-06-30 23:59:59'}
+        data = {'title':'Test Task', 'due_at':'2023-06-30 23:59:59'}
         response = client.post('/', data)
 
         self.assertEqual(response.status_code, 200)
